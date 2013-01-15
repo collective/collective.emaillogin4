920653a8e3f5ecc3b2b96a575ffd812891fe0299
Maurits van Rees <maurits@vanrees.org>
2013-01-15 12:13:12 +0100

    Add test_doctests.py

---
+++ collective/emaillogin4/patches/pa_users/tests/test_doctests.py
@@ -0,0 +1,44 @@
+import doctest
+from unittest import TestSuite
+
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+from Products.PloneTestCase.PloneTestCase import setupPloneSite
+
+from plone.app.users.tests import TestCase
+
+setupPloneSite()
+
+OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+
+
+class DocTestCase(TestCase):
+    # just here to work around a weird error message
+    pass
+
+
+def test_suite():
+    # These are original, unchanged tests.
+    tests = ['flexible_user_registration.txt',
+             'forms_navigationroot.txt',
+             'registration_forms.txt',
+             'userdata.txt',
+             'userdata_prefs_user_details.txt',
+             'personal_preferences.txt',
+             'personal_preferences_prefs_user_details.txt',
+             'password.txt',
+             ]
+    suite = TestSuite()
+    for test in tests:
+        suite.addTest(FunctionalDocFileSuite(test,
+            optionflags=OPTIONFLAGS,
+            package="plone.app.users.tests",
+            test_class=DocTestCase))
+    # This is a new one.
+    tests = ['email_login.txt',
+             ]
+    for test in tests:
+        suite.addTest(FunctionalDocFileSuite(test,
+            optionflags=OPTIONFLAGS,
+            package="collective.emaillogin4.patches.pa_users.tests",
+            test_class=DocTestCase))
+    return suite
